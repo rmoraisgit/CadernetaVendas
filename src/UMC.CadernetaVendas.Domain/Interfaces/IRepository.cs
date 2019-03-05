460a7da8693ce53d61f0c0b3dd2f66849ba603d0
rmoraisgit <rafamorais.br@live.com>
2019-03-05 16:14:02 -0300

    Adicionado contratos de repositorio generico e especializado para Produto.

---
+++ src/UMC.CadernetaVendas.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using UMC.CadernetaVendas.Domain.Core.Models;
+
+namespace UMC.CadernetaVendas.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : Entity<TEntity>
+    {
+        void Adicionar(TEntity obj);
+        TEntity ObterPorId(Guid id);
+        IEnumerable<TEntity> ObterTodos();
+        void Atualizar(TEntity obj);
+        void Remover(Guid id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicate);
+        int SaveChanges();
+    }
+}
