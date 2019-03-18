4d4d46b55dacd96f2dcaadcc687564022371f2e9
rmoraisgit <rafamorais.br@live.com>
2019-03-17 21:59:28 -0300

    Iniciado desenvolvimento da tela de cadastro de Produto.

--- client/CadernetaVendas/src/app/produtos/adicionar-produto/adicionar-produto.component.ts
+++ client/CadernetaVendas/src/app/produtos/adicionar-produto/adicionar-produto.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'cv-adicionar-produto',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdicionarProdutoComponent implements OnInit {
 
-  constructor() { }
+  produtoForm: FormGroup;
+  importarArquivoForm: FormGroup;
+  fileToUpload: File = null;
+
+  categoriaSelecionada: string = '';
+
+  @ViewChild('labelImport')
+  labelImport: ElementRef;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+
+    this.importarArquivoForm = this.formBuilder.group({
+      importFile: ['', Validators.required]
+    });
+
+    this.produtoForm = this.formBuilder.group({
+      importFile: ['', Validators.required],
+      Nome: ['', Validators.required]
+    });
   }
 
+  onFileChange(files: FileList) {
+    this.labelImport.nativeElement.innerText = Array.from(files)
+      .map(f => f.name)
+      .join(', ');
+    this.fileToUpload = files.item(0);
+  }
+
+  tioR(categoriaSelecionada: string){
+    console.log("OI")
+    console.log(categoriaSelecionada);
+
+    if(categoriaSelecionada == 'Banho'){
+      console.log("OI TIO RAFAEL")
+
+      this.categoriaSelecionada = categoriaSelecionada;
+
+      console.log(categoriaSelecionada)
+      
+      // this.produtoForm = this.formBuilder.group({
+      //   importFile: ['', Validators.required],
+      //   Nome: ['', Validators.required]
+      // });
+    }
+
+  }
 }
--- client/CadernetaVendas/src/app/produtos/produtos.module.ts
+++ client/CadernetaVendas/src/app/produtos/produtos.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AdicionarProdutoComponent } from './adicionar-produto/adicionar-produto.component';
 import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
@@ -10,7 +11,9 @@ import { ListaProdutosComponent } from './lista-produtos/lista-produtos.componen
     ListaProdutosComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
   ]
 })
 export class ProdutosModule { }
