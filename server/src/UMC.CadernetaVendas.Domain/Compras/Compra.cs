2c9ced9a089d8059eb51d73258b4c7c96b51ec4d
rmoraisgit <rafamorais.br@live.com>
2019-04-27 15:15:06 -0300

    Iniciada construcao do modulo de compras.

---
+++ server/src/UMC.CadernetaVendas.Domain/Compras/Compra.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UMC.CadernetaVendas.Domain.Core.Models;
+using UMC.CadernetaVendas.Domain.Produtos;
+
+namespace UMC.CadernetaVendas.Domain.Compras
+{
+    public class Compra : Entity<Compra>
+    {
+        public string Fornecedor { get; private set; }
+        public DateTime DataCompra { get; private set; }
+        public decimal Total { get; private set; }
+        public virtual ICollection<Produto> Produtos { get; private set; }
+
+        public override bool EhValido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
